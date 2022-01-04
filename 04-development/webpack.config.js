ddd90c3af4636973b46ea6905f5652d3a14fcd2c
ACDC5 <ricof6f@gmail.com>
2022-01-04 12:02:01 +0800

    完成04-development章节相关的学习

--- 04-development/webpack.config.js
+++ 04-development/webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
         clean:true //打包时，清除上一次打包生成的文件
     },
 
-    mode: 'none',
+    mode: 'development', //使webpack模式变为开发者模式
+    devtool: 'inline-source-map',//当代码报错时，在控制台输出具体报错的位置，而不是定位到编译好后的bundle文件中
 
     plugins: [
         // 使用HtmlWebpackPlugin插件,该插件自动帮我们引入打包好的文件到index.html
@@ -21,5 +22,14 @@ module.exports = {
             filename:'app.html',//将打包后生成的文件放在此文件(没有则会创建该文件)中展示
             inject: 'body'//将打包后的文件具体放在body标签中
         })
-    ]
+    ],
+
+    // https://www.bilibili.com/video/BV1YU4y1g745?p=20
+    devServer: {
+        // 指定devServer的根目录
+        static: './dist'
+
+        // npx webpack-dev-server命令实际没有输出任何物理文件，
+        // 他把打包好的bundle文件放在了内存里,就算把指定的dist文件删除也不会影响页面的展示(物理删除不影响内存中打包好的文件)
+    }
 }
